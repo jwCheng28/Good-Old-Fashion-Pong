02354801e5c9cce9337c1aa53ac210b14880a360
jwCheng28 <jwCheng28@users.noreply.github.com>
2021-08-04 22:19:33 -0700

    Added threading for updating values, might change up later

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
 #include <string>
 #include "game.hpp"
+#include <SDL2/SDL_thread.h>
+#include <SDL2/SDL.h>
+
+volatile int started = 0;
+
+int updatePos(void *obj) {
+    while (!started);
+    while (!((Game*) obj)->ended()) {
+        ((Game*) obj)->update(1.0/30.0);
+        SDL_Delay(1);
+    }
+    return 0;
+}
 
 int main(int argc, char* args[]) {
     bool AI = (argc > 1 && args[1] == std::string("True"));
     Game *game = new Game("Pong", 1400, 800, AI);
 
-    while (!(game -> ended())) {
-        game -> eventHandler();
-        game -> update(1.0/30.0);
-        game -> render();
+    SDL_CreateThread(updatePos, "Updater", game);
+    while (!(game->ended())) {
+        started = 1;
+        game->eventHandler();
+        // game->update(1.0/30.0);
+        game->render();
     }
     delete game;
     return 0;
