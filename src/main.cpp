fe8ac8d264bd00ae24e78257d2cbacfaf932fef9
jwCheng28 <jwCheng28@users.noreply.github.com>
2021-08-06 17:51:52 -0700

    Removed implementation with extra thread

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,13 @@
 #include <SDL2/SDL_thread.h>
 #include <SDL2/SDL.h>
 
-volatile int started = 0;
-
-int updatePos(void *obj) {
-    while (!started);
-    while (!((Game*) obj)->ended()) {
-        ((Game*) obj)->update(1.0/30.0);
-        SDL_Delay(1);
-    }
-    return 0;
-}
-
 int main(int argc, char* args[]) {
     bool AI = (argc > 1 && args[1] == std::string("True"));
     Game *game = new Game("Pong", 1400, 800, AI);
 
-    SDL_CreateThread(updatePos, "Updater", game);
     while (!(game->ended())) {
-        started = 1;
         game->eventHandler();
-        // game->update(1.0/30.0);
+        game->update(1.0/30.0);
         game->render();
     }
     delete game;
